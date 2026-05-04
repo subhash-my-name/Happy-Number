4d3fa0376f1f04fcdf16cb4ca49cd33f0baf7af4
Subhash <subhashbishnoi410@protonmail.com>
2026-05-04 22:25:42 +0530

    happyNum.c

--- happyNum.c
+++ happyNum.c
@@ -20,6 +20,11 @@ int isHappy (int n)
 	
 	int sum = 0, r;
 
+	// upon investigation, i found that numbers end up at 37, and 37 loops back to 37 itself.
+	// therefore i concluded that sum ending up as 37 is the case for unhappy numbers.
+	// although it is not mathematically rigorous, i had no clue how to solve this question.
+	// therefore, to find a pattern, i picked random multi-digit numbers and those who were not happy,
+	// ended up at 37. so i simply assumed that 37 is the mother of sadness.
 	while(sum != 1 && sum != 37)
 	{
 		sum = dSum(n);
